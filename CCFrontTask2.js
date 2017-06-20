439968b1877d2f948b80186db4fa84c093f495c0
Tomasz Pięta <tomaszpieta@na-sieci.pl>
2017-06-20 15:04:22 +0200

    Add files via upload

--- CCFrontTask2.js
+++ CCFrontTask2.js
@@ -1,23 +1,47 @@
 function damage(spell) {
-var damage = [];
-var keys = [];
-var values = [];
-var fe = (spell.match(/fe/g) || []).length;
-spell = spell.slice(spell.indexOf("fe"),spell.lastIndexOf("ai")+2);
-var subspells = {"fe":1,"jee":3,"je":2,"ai":2,"ain":3,"dai":5,"ne":2};
-keys = Object.keys(subspells);
-values = Object.values(subspells);
+	// declaring variables
+	var damage = 0;
+	var keys = [];
+	var values = [];
+	var fe = (spell.match(/fe/g)||[]).length;
+	var subspells = {"fe":1,"jee":3,"je":2,"ai":2,"ain":3,"dai":5,"ne":2};
+	var keys = Object.keys(subspells);
+	var values = Object.values(subspells);
 
-if(fe > 1){
-	damage = 0; alert(damage);}
-else{j = 1;
-  while (j <= spell.length) {j++;
-		for (i = 0; i < keys.length; i++){
-    	if(spell.indexOf(keys[i]) >= 0){
-      	spell = spell.replace(keys[i],"");
-        damage = parseInt(damage + values[i]);}}}
-  damage = damage - spell.length;
-  if(damage < 0){damage = 0;}
-  alert(damage);}}
+	// spell start with 'fe' and end with 'ai', so it has been sliced to that form
+	spell = spell.slice(spell.indexOf("fe"),spell.lastIndexOf("ai")+2);
+
+	// 'fe' can occur only once, any spells with 'fe' occurence more than 1 is incorrect, ergo damage equals 0
+	if(fe > 1){
+		damage = 0;
+		alert(damage);
+	}
+	else{
+		var j = 1;
+		// while for loop have done iterating through all subspells and there are some left
+		while (j <= spell.length){
+			// could have been done by counting how many times subspell occurs in spell
+			// var ai_count = (spell.match(new RegExp(ai,"g"))||[]).length;
+			// but I wanted my code short and for(var i in subspells) resulted the same
+			j++;
+			for(var i in subspells){
+				// if subspell occurs in spell, if indexOf is not negative
+				if(spell.indexOf(i) >= 0){
+					// spell cuts off subspell
+					spell = spell.replace(i,"");
+					// and damage is magnified by value of subspell
+					damage += subspells[i];
+				}
+			}
+		}
+		// damage is reduced by value how many other letters have left
+		damage -= spell.length;
+		// should return 0 if damage is negative
+		if(damage < 0){
+			damage = 0;
+		}
+		alert(damage);
+	}
+}
 
 var spell = "fejejeeaindaiyaiai"; damage(spell);
